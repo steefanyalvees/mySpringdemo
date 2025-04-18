fb1d83516e8ba1057defc4573ab69d69dc93386f
steefanyalvees <stefanyanjoos@hotmail.com>
2025-04-18 19:45:04 +0100

    initial commit

---
+++ src/main/java/com/example/myspringdemo/MySpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringdemoApplication.class, args);
+    }
+
+}
